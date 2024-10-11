c28ebae2069fc2565356ffcfe68852cb52181390
lv10wizard <>
2024-10-11 15:21:10 -0700

    fix!: TorrentSetArgs types to match transmission
    BREAKING CHANGE: Changes the types of various fields in TorrentSetArgs
    to mirror the types defined in [1] `transmission.h` (as of Aug 12, 2024:
    [2] dbea328).
    
    * Refactors IdleMode, RatioMode from response.rs -> request.rs
    * Fixes TorrentSetArgs setter extraneous allocations
    
    [1] https://github.com/transmission/transmission/blob/dbea32809ae7b75d52d236ebe1e6a4739d453349/libtransmission/transmission.h
    [2] https://github.com/transmission/transmission/commit/dbea32809ae7b75d52d236ebe1e6a4739d453349

--- src/types/response.rs
+++ src/types/response.rs
@@ -14,7 +14,7 @@ use serde::Deserialize;
 use serde_json::Value;
 use serde_repr::*;
 
-use crate::types::request::Priority;
+use crate::types::request::{IdleMode, Priority, RatioMode};
 use crate::types::Id;
 
 #[derive(Deserialize, Debug)]
@@ -369,22 +369,6 @@ pub struct PeersFrom {
     pub from_ltep: u16,
     pub from_pex: u16,
     pub from_tracker: u16,
-}
-
-#[derive(Deserialize_repr, Debug, Copy, Clone, PartialEq)]
-#[repr(i8)]
-pub enum IdleMode {
-    Global = 0,
-    Single = 1,
-    Unlimited = 2,
-}
-
-#[derive(Deserialize_repr, Debug, Copy, Clone, PartialEq)]
-#[repr(i8)]
-pub enum RatioMode {
-    Global = 0,
-    Single = 1,
-    Unlimited = 2,
 }
 
 #[derive(Deserialize, Debug, Clone)]
