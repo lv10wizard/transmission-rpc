18e9559b1e9c9d720b56498f36835a144a124566
lv10wizard <foo@example.com>
2026-03-18 11:42:05 -0700

    test: add session-get legacy and compat ser tests
    * Added SessionGetField::PreferredTransports
    
    * Fixed SessionGetField::ScriptTorrentAddedEnabled PascalCase naming
      (was Scripttorrentaddedenabled)

--- src/types/request/session_get.rs
+++ src/types/request/session_get.rs
@@ -120,6 +120,7 @@ pub enum SessionGetField {
     PeerPort,
     PexEnabled,
     PortForwardingEnabled,
+    PreferredTransports,
     QueueStalledEnabled,
     QueueStalledMinutes,
     RenamePartialFiles,
@@ -127,7 +128,7 @@ pub enum SessionGetField {
     RpcVersionMinimum,
     RpcVersionSemver,
     RpcVersion,
-    Scripttorrentaddedenabled,
+    ScriptTorrentAddedEnabled,
     ScriptTorrentAddedFilename,
     ScriptTorrentDoneEnabled,
     ScriptTorrentDoneFilename,
@@ -139,7 +140,7 @@ pub enum SessionGetField {
     SeedRatioLimit,
     #[serde(rename = "seedRatioLimited")]
     SeedRatioLimited,
-    #[serde(rename = "sequential_download")]
+    #[serde(rename = "sequential_download")] // Doesn't exist pre- semver-6.0.0
     SequentialDownload,
     SessionId,
     SpeedLimitDownEnabled,
@@ -320,5 +321,502 @@ mod serde_tests {
     #[test]
     fn request_session_get_semver_600_cache_size_mb() -> Result<()> {
         semver_600_test([SessionGetField::CacheSizeMb], "cache_size_mib")
+    }
+
+    #[test]
+    fn request_session_get_legacy_config_dir() -> Result<()> {
+        legacy_test([SessionGetField::ConfigDir], "config-dir")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_config_dir() -> Result<()> {
+        semver_600_test([SessionGetField::ConfigDir], "config_dir")
+    }
+
+    #[test]
+    fn request_session_get_legacy_default_trackers() -> Result<()> {
+        legacy_test([SessionGetField::DefaultTrackers], "default-trackers")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_default_trackers() -> Result<()> {
+        semver_600_test([SessionGetField::DefaultTrackers], "default_trackers")
+    }
+
+    #[test]
+    fn request_session_get_legacy_dht_enabled() -> Result<()> {
+        legacy_test([SessionGetField::DhtEnabled], "dht-enabled")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_dht_enabled() -> Result<()> {
+        semver_600_test([SessionGetField::DhtEnabled], "dht_enabled")
+    }
+
+    #[test]
+    fn request_session_get_legacy_download_dir() -> Result<()> {
+        legacy_test([SessionGetField::DownloadDir], "download-dir")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_download_dir() -> Result<()> {
+        semver_600_test([SessionGetField::DownloadDir], "download_dir")
+    }
+
+    #[test]
+    fn request_session_get_legacy_download_dir_free_space() -> Result<()> {
+        legacy_test([SessionGetField::DownloadDirFreeSpace], "download-dir-free-space")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_download_dir_free_space() -> Result<()> {
+        semver_600_test([SessionGetField::DownloadDirFreeSpace], "download_dir_free_space")
+    }
+
+    #[test]
+    fn request_session_get_legacy_download_queue_enabled() -> Result<()> {
+        legacy_test([SessionGetField::DownloadQueueEnabled], "download-queue-enabled")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_download_queue_enabled() -> Result<()> {
+        semver_600_test([SessionGetField::DownloadQueueEnabled], "download_queue_enabled")
+    }
+
+    #[test]
+    fn request_session_get_legacy_download_queue_size() -> Result<()> {
+        legacy_test([SessionGetField::DownloadQueueSize], "download-queue-size")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_download_queue_size() -> Result<()> {
+        semver_600_test([SessionGetField::DownloadQueueSize], "download_queue_size")
+    }
+
+    #[test]
+    fn request_session_get_legacy_encryption() -> Result<()> {
+        legacy_test([SessionGetField::Encryption], "encryption")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_encryption() -> Result<()> {
+        semver_600_test([SessionGetField::Encryption], "encryption")
+    }
+
+    #[test]
+    fn request_session_get_legacy_idle_seeding_limit() -> Result<()> {
+        legacy_test([SessionGetField::IdleSeedingLimit], "idle-seeding-limit")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_idle_seeding_limit() -> Result<()> {
+        semver_600_test([SessionGetField::IdleSeedingLimit], "idle_seeding_limit")
+    }
+
+    #[test]
+    fn request_session_get_legacy_idle_seeding_limit_enabled() -> Result<()> {
+        legacy_test([SessionGetField::IdleSeedingLimitEnabled], "idle-seeding-limit-enabled")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_idle_seeding_limit_enabled() -> Result<()> {
+        semver_600_test([SessionGetField::IdleSeedingLimitEnabled], "idle_seeding_limit_enabled")
+    }
+
+    #[test]
+    fn request_session_get_legacy_incomplete_dir() -> Result<()> {
+        legacy_test([SessionGetField::IncompleteDir], "incomplete-dir")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_incomplete_dir() -> Result<()> {
+        semver_600_test([SessionGetField::IncompleteDir], "incomplete_dir")
+    }
+
+    #[test]
+    fn request_session_get_legacy_incomplete_dir_enabled() -> Result<()> {
+        legacy_test([SessionGetField::IncompleteDirEnabled], "incomplete-dir-enabled")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_incomplete_dir_enabled() -> Result<()> {
+        semver_600_test([SessionGetField::IncompleteDirEnabled], "incomplete_dir_enabled")
+    }
+
+    #[test]
+    fn request_session_get_legacy_lpd_enabled() -> Result<()> {
+        legacy_test([SessionGetField::LpdEnabled], "lpd-enabled")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_lpd_enabled() -> Result<()> {
+        semver_600_test([SessionGetField::LpdEnabled], "lpd_enabled")
+    }
+
+    #[test]
+    fn request_session_get_legacy_peer_limit_global() -> Result<()> {
+        legacy_test([SessionGetField::PeerLimitGlobal], "peer-limit-global")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_peer_limit_global() -> Result<()> {
+        semver_600_test([SessionGetField::PeerLimitGlobal], "peer_limit_global")
+    }
+
+    #[test]
+    fn request_session_get_legacy_peer_limit_per_torrent() -> Result<()> {
+        legacy_test([SessionGetField::PeerLimitPerTorrent], "peer-limit-per-torrent")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_peer_limit_per_torrent() -> Result<()> {
+        semver_600_test([SessionGetField::PeerLimitPerTorrent], "peer_limit_per_torrent")
+    }
+
+    #[test]
+    fn request_session_get_legacy_peer_port_random_on_start() -> Result<()> {
+        legacy_test([SessionGetField::PeerPortRandomOnStart], "peer-port-random-on-start")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_peer_port_random_on_start() -> Result<()> {
+        semver_600_test([SessionGetField::PeerPortRandomOnStart], "peer_port_random_on_start")
+    }
+
+    #[test]
+    fn request_session_get_legacy_peer_port() -> Result<()> {
+        legacy_test([SessionGetField::PeerPort], "peer-port")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_peer_port() -> Result<()> {
+        semver_600_test([SessionGetField::PeerPort], "peer_port")
+    }
+
+    #[test]
+    fn request_session_get_legacy_pex_enabled() -> Result<()> {
+        legacy_test([SessionGetField::PexEnabled], "pex-enabled")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_pex_enabled() -> Result<()> {
+        semver_600_test([SessionGetField::PexEnabled], "pex_enabled")
+    }
+
+    #[test]
+    fn request_session_get_legacy_port_forwarding_enabled() -> Result<()> {
+        legacy_test([SessionGetField::PortForwardingEnabled], "port-forwarding-enabled")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_port_forwarding_enabled() -> Result<()> {
+        semver_600_test([SessionGetField::PortForwardingEnabled], "port_forwarding_enabled")
+    }
+
+    #[test]
+    fn request_session_get_legacy_preferred_transports() -> Result<()> {
+        legacy_test([SessionGetField::PreferredTransports], "preferred-transports")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_preferred_transports() -> Result<()> {
+        semver_600_test([SessionGetField::PreferredTransports], "preferred_transports")
+    }
+
+    #[test]
+    fn request_session_get_legacy_queue_stalled_enabled() -> Result<()> {
+        legacy_test([SessionGetField::QueueStalledEnabled], "queue-stalled-enabled")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_queue_stalled_enabled() -> Result<()> {
+        semver_600_test([SessionGetField::QueueStalledEnabled], "queue_stalled_enabled")
+    }
+
+    #[test]
+    fn request_session_get_legacy_queue_stalled_minutes() -> Result<()> {
+        legacy_test([SessionGetField::QueueStalledMinutes], "queue-stalled-minutes")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_queue_stalled_minutes() -> Result<()> {
+        semver_600_test([SessionGetField::QueueStalledMinutes], "queue_stalled_minutes")
+    }
+
+    #[test]
+    fn request_session_get_legacy_rename_partial_files() -> Result<()> {
+        legacy_test([SessionGetField::RenamePartialFiles], "rename-partial-files")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_rename_partial_files() -> Result<()> {
+        semver_600_test([SessionGetField::RenamePartialFiles], "rename_partial_files")
+    }
+
+    #[test]
+    fn request_session_get_legacy_reqq() -> Result<()> {
+        legacy_test([SessionGetField::Reqq], "reqq")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_reqq() -> Result<()> {
+        semver_600_test([SessionGetField::Reqq], "reqq")
+    }
+
+    #[test]
+    fn request_session_get_legacy_rpc_version_minimum() -> Result<()> {
+        legacy_test([SessionGetField::RpcVersionMinimum], "rpc-version-minimum")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_rpc_version_minimum() -> Result<()> {
+        semver_600_test([SessionGetField::RpcVersionMinimum], "rpc_version_minimum")
+    }
+
+    #[test]
+    fn request_session_get_legacy_rpc_version_semver() -> Result<()> {
+        legacy_test([SessionGetField::RpcVersionSemver], "rpc-version-semver")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_rpc_version_semver() -> Result<()> {
+        semver_600_test([SessionGetField::RpcVersionSemver], "rpc_version_semver")
+    }
+
+    #[test]
+    fn request_session_get_legacy_rpc_version() -> Result<()> {
+        legacy_test([SessionGetField::RpcVersion], "rpc-version")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_rpc_version() -> Result<()> {
+        semver_600_test([SessionGetField::RpcVersion], "rpc_version")
+    }
+
+    #[test]
+    fn request_session_get_legacy_script_torrent_added_enabled() -> Result<()> {
+        legacy_test([SessionGetField::ScriptTorrentAddedEnabled], "script-torrent-added-enabled")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_script_torrent_added_enabled() -> Result<()> {
+        semver_600_test(
+            [SessionGetField::ScriptTorrentAddedEnabled],
+            "script_torrent_added_enabled")
+    }
+
+    #[test]
+    fn request_session_get_legacy_script_torrent_added_filename() -> Result<()> {
+        legacy_test([SessionGetField::ScriptTorrentAddedFilename], "script-torrent-added-filename")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_script_torrent_added_filename() -> Result<()> {
+        semver_600_test(
+            [SessionGetField::ScriptTorrentAddedFilename],
+            "script_torrent_added_filename")
+    }
+
+    #[test]
+    fn request_session_get_legacy_script_torrent_done_enabled() -> Result<()> {
+        legacy_test([SessionGetField::ScriptTorrentDoneEnabled], "script-torrent-done-enabled")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_script_torrent_done_enabled() -> Result<()> {
+        semver_600_test(
+            [SessionGetField::ScriptTorrentDoneEnabled],
+            "script_torrent_done_enabled")
+    }
+
+    #[test]
+    fn request_session_get_legacy_script_torrent_done_filename() -> Result<()> {
+        legacy_test([SessionGetField::ScriptTorrentDoneFilename], "script-torrent-done-filename")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_script_torrent_done_filename() -> Result<()> {
+        semver_600_test(
+            [SessionGetField::ScriptTorrentDoneFilename],
+            "script_torrent_done_filename")
+    }
+
+    #[test]
+    fn request_session_get_legacy_script_torrent_done_seeding_enabled() -> Result<()> {
+        legacy_test(
+            [SessionGetField::ScriptTorrentDoneSeedingEnabled],
+            "script-torrent-done-seeding-enabled")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_script_torrent_done_seeding_enabled() -> Result<()> {
+        semver_600_test(
+            [SessionGetField::ScriptTorrentDoneSeedingEnabled],
+            "script_torrent_done_seeding_enabled")
+    }
+    #[test]
+    fn request_session_get_legacy_script_torrent_done_seeding_filename() -> Result<()> {
+        legacy_test(
+            [SessionGetField::ScriptTorrentDoneSeedingFilename],
+            "script-torrent-done-seeding-filename")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_script_torrent_done_seeding_filename() -> Result<()> {
+        semver_600_test(
+            [SessionGetField::ScriptTorrentDoneSeedingFilename],
+            "script_torrent_done_seeding_filename")
+    }
+
+    #[test]
+    fn request_session_get_legacy_seed_queue_enabled() -> Result<()> {
+        legacy_test([SessionGetField::SeedQueueEnabled], "seed-queue-enabled")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_seed_queue_enabled() -> Result<()> {
+        semver_600_test([SessionGetField::SeedQueueEnabled], "seed_queue_enabled")
+    }
+
+    #[test]
+    fn request_session_get_legacy_seed_queue_size() -> Result<()> {
+        legacy_test([SessionGetField::SeedQueueSize], "seed-queue-size")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_seed_queue_size() -> Result<()> {
+        semver_600_test([SessionGetField::SeedQueueSize], "seed_queue_size")
+    }
+
+    #[test]
+    fn request_session_get_legacy_seed_ratio_limit() -> Result<()> {
+        legacy_test([SessionGetField::SeedRatioLimit], "seedRatioLimit")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_seed_ratio_limit() -> Result<()> {
+        semver_600_test([SessionGetField::SeedRatioLimit], "seed_ratio_limit")
+    }
+
+    #[test]
+    fn request_session_get_legacy_seed_ratio_limited() -> Result<()> {
+        legacy_test([SessionGetField::SeedRatioLimited], "seedRatioLimited")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_seed_ratio_limited() -> Result<()> {
+        semver_600_test([SessionGetField::SeedRatioLimited], "seed_ratio_limited")
+    }
+
+    #[test]
+    fn request_session_get_legacy_sequential_download() -> Result<()> {
+        legacy_test([SessionGetField::SequentialDownload], "sequential_download")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_sequential_download() -> Result<()> {
+        semver_600_test([SessionGetField::SequentialDownload], "sequential_download")
+    }
+
+    #[test]
+    fn request_session_get_legacy_session_id() -> Result<()> {
+        legacy_test([SessionGetField::SessionId], "session-id")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_session_id() -> Result<()> {
+        semver_600_test([SessionGetField::SessionId], "session_id")
+    }
+
+    #[test]
+    fn request_session_get_legacy_speed_limit_down_enabled() -> Result<()> {
+        legacy_test([SessionGetField::SpeedLimitDownEnabled], "speed-limit-down-enabled")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_speed_limit_down_enabled() -> Result<()> {
+        semver_600_test([SessionGetField::SpeedLimitDownEnabled], "speed_limit_down_enabled")
+    }
+
+    #[test]
+    fn request_session_get_legacy_speed_limit_down() -> Result<()> {
+        legacy_test([SessionGetField::SpeedLimitDown], "speed-limit-down")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_speed_limit_down() -> Result<()> {
+        semver_600_test([SessionGetField::SpeedLimitDown], "speed_limit_down")
+    }
+
+    #[test]
+    fn request_session_get_legacy_speed_limit_up_enabled() -> Result<()> {
+        legacy_test([SessionGetField::SpeedLimitUpEnabled], "speed-limit-up-enabled")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_speed_limit_up_enabled() -> Result<()> {
+        semver_600_test([SessionGetField::SpeedLimitUpEnabled], "speed_limit_up_enabled")
+    }
+
+    #[test]
+    fn request_session_get_legacy_speed_limit_up() -> Result<()> {
+        legacy_test([SessionGetField::SpeedLimitUp], "speed-limit-up")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_speed_limit_up() -> Result<()> {
+        semver_600_test([SessionGetField::SpeedLimitUp], "speed_limit_up")
+    }
+
+    #[test]
+    fn request_session_get_legacy_start_added_torrents() -> Result<()> {
+        legacy_test([SessionGetField::StartAddedTorrents], "start-added-torrents")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_start_added_torrents() -> Result<()> {
+        semver_600_test([SessionGetField::StartAddedTorrents], "start_added_torrents")
+    }
+
+    #[test]
+    fn request_session_get_legacy_trash_original_torrent_files() -> Result<()> {
+        legacy_test([SessionGetField::TrashOriginalTorrentFiles], "trash-original-torrent-files")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_trash_original_torrent_files() -> Result<()> {
+        semver_600_test(
+            [SessionGetField::TrashOriginalTorrentFiles],
+            "trash_original_torrent_files")
+    }
+
+    #[test]
+    fn request_session_get_legacy_units() -> Result<()> {
+        legacy_test([SessionGetField::Units], "units")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_units() -> Result<()> {
+        semver_600_test([SessionGetField::Units], "units")
+    }
+
+    #[test]
+    fn request_session_get_legacy_utp_enabled() -> Result<()> {
+        legacy_test([SessionGetField::UtpEnabled], "utp-enabled")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_utp_enabled() -> Result<()> {
+        semver_600_test([SessionGetField::UtpEnabled], "utp_enabled")
+    }
+
+    #[test]
+    fn request_session_get_legacy_version() -> Result<()> {
+        legacy_test([SessionGetField::Version], "version")
+    }
+
+    #[test]
+    fn request_session_get_semver_600_version() -> Result<()> {
+        semver_600_test([SessionGetField::Version], "version")
     }
 }
