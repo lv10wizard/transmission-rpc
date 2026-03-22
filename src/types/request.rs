bceb889e7d0d1b7d1be89ad3b5a7b2984fb47bd5
lv10wizard <foo@example.com>
2026-03-21 20:17:56 -0700

    refactor(torrent_set): move TorrentSetArgs to its own file

--- src/types/request.rs
+++ src/types/request.rs
@@ -7,15 +7,15 @@ use serde_with::skip_serializing_none;
 use compat_macros::GenerateCompat;
 
 use crate::json_rpc::{JsonRpcId, JsonRpcRequest};
-use super::{
-    AltSpeedDay, Encryption, EncryptionCompat, Id, IdleMode, Priority, RatioMode, Tag, Transport,
-};
+use super::{AltSpeedDay, Encryption, EncryptionCompat, Id, Priority, Tag, Transport};
 
 pub(crate) use session_get::*; // SessionGetArgs, __semver_600_compat_SessionGetArgs
 pub(crate) use torrent_get::*; // TorrentGetArgs, __semver_600_compat_TorrentGetArgs
+pub(crate) use torrent_set::*; // TorrentSetArgs, __semver_600_compat_TorrentSetArgs
 
 pub use session_get::SessionGetField;
 pub use torrent_get::TorrentGetField;
+pub use torrent_set::TorrentSetArgs;
 
 mod group_set;
 mod into;
@@ -1005,161 +1005,6 @@ impl Serialize for TrackerList {
     {
         self.0.join("\n").serialize(serializer)
     }
-}
-
-/// Defines request arguments for the [`torrent_set`] method.
-///
-/// # Constructors
-///
-/// * [`TorrentSetArgs::default`] creates a new [`TorrentSetArgs`] instance with all fields set to
-/// their default value (`None`).
-/// * [`TorrentSetArgs::new`] is an alias for [`TorrentSetArgs::default`].
-///
-/// # Setters
-///
-/// These methods are fluent setters, returning a new [`TorrentSetArgs`] instance modifying only
-/// the corresponding field while leaving all other fields untouched.
-///
-/// * [`TorrentSetArgs::bandwidth_priority`]: The torrents' bandwidth [`Priority`].
-/// * [`TorrentSetArgs::download_limit`]: Maximum download speed (`KBps`).
-/// * [`TorrentSetArgs::download_limited`]: `true` to honor `download_limit`.
-/// * [`TorrentSetArgs::files_wanted`]: Indices of file(s) to download.
-/// * [`TorrentSetArgs::files_unwanted`]: Indices of file(s) to skip (ie, not download).
-/// * [`TorrentSetArgs::group`]: The name of the torrents' bandwidth group.
-///     > Added in Transmission 4.0.0 (`rpc-version-semver` 5.3.0, `rpc-version`: 17).
-/// * [`TorrentSetArgs::honors_session_limits`]: `true` to honor the session's upload limits.
-/// * [`TorrentSetArgs::labels`]: A `Vec` of `String` labels to set on the torrent(s).
-///     > Added in Transmission 3.00 (`rpc-version-semver` 5.2.0, `rpc-version`: 16).
-/// * [`TorrentSetArgs::location`]: The new location of the torrents' content.
-/// * [`TorrentSetArgs::peer_limit`]: Maximum number of peers.
-/// * [`TorrentSetArgs::priority_high`]: Indices of [`Priority::High`] file(s).
-/// * [`TorrentSetArgs::priority_low`]: Indices of [`Priority::Low`] file(s).
-/// * [`TorrentSetArgs::priority_normal`]: Indices of [`Priority::Normal`] file(s).
-/// * [`TorrentSetArgs::queue_position`]: The new queue position of this torrent `[0..n)`.
-/// * [`TorrentSetArgs::seed_idle_limit`]: Torrent-level number of minutes of seeding inactivity
-/// before it considered `stalled`.
-/// * [`TorrentSetArgs::seed_idle_mode`]: Which seeding inactivity mode ([`IdleMode`]) to use.
-/// * [`TorrentSetArgs::seed_ratio_limit`]: Torrent-level seeding ratio.
-/// * [`TorrentSetArgs::seed_ratio_mode`]: Which [`RatioMode`] to use.
-/// * [`TorrentSetArgs::sequential_download`]: `true` to download the torrent pieces sequentially.
-///     > Added in Transmission 4.1.0 (`rpc-version-semver` 6.0.0, `rpc-version`: 18).
-/// * [`TorrentSetArgs::tracker_add`]: Add a new tracker url in its own new tier.
-///     * *NOTE:* This documentation may be incorrect. The rpc-spec itself is unclear.
-///     > ⚠ Deprecated in Transmission 4.0.0 (`rpc-version-semver` 5.3.0, `rpc-version`: 17);
-///     > prefer `tracker_list` if possible.
-/// * [`TorrentSetArgs::tracker_list`]: `TrackerList` of announce urls with an empty element
-/// between [tiers](https://www.bittorrent.org/beps/bep_0012.html).
-///     > Added in Transmission 4.0.0 (`rpc-version-semver` 5.3.0, `rpc-version`: 17).
-/// * [`TorrentSetArgs::tracker_remove`]: [`Trackers::id`] of trackers to remove.
-///     * *NOTE:* This documentation may be incorrect. The rpc-spec itself is unclear.
-///     > ⚠ Deprecated in Transmission 4.0.0 (`rpc-version-semver` 5.3.0, `rpc-version`: 17);
-///     > prefer `tracker_list` if possible.
-/// * [`TorrentSetArgs::tracker_replace`]: Pairs of <[`Trackers::id`]/new announce urls>.
-///     * *NOTE:* This documentation may be incorrect. The rpc-spec itself is unclear.
-///     * See: transmission/transmission
-///     [#3226](https://github.com/transmission/transmission/issues/3226#issuecomment-1411899883).
-///     > ⚠ Deprecated in Transmission 4.0.0 (`rpc-version-semver` 5.3.0, `rpc-version`: 17);
-///     > prefer `tracker_list` if possible.
-/// * [`TorrentSetArgs::upload_limit`]: Maximum upload speed (`KBps`).
-/// * [`TorrentSetArgs::upload_limited`]: `true` to honor `upload_limit`.
-///
-/// # Examples
-///
-/// With fluent setters:
-/// ```
-/// use transmission_rpc::types::TorrentSetArgs;
-///
-/// let args = TorrentSetArgs()::default()
-///                .seed_ratio_limit(12.34)
-///                .labels(vec!["foo", "bar"])
-///                .locations("/a/b/c/d");
-/// ```
-///
-/// Directly setting struct fields:
-/// ```
-/// use transmission_rpc::types::TorrentSetArgs;
-///
-/// let mut args = TorrentSetArgs()::default();
-/// args.seed_ratio_limit = Some(12.34);
-/// args.labels = Some(vec!["foo", "bar"]);
-/// args.locations = Some("/a/b/c/d");
-/// ```
-///
-/// [`torrent_set`]: crate::TransClient::torrent_set
-/// [`Trackers::id`]: super::Trackers::id
-#[derive(GenerateCompat, Serialize, Debug, Clone, Default, PartialEq)]
-#[serde(rename_all = "camelCase")]
-pub struct TorrentSetArgs {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub bandwidth_priority: Option<Priority>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub download_limit: Option<usize>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub download_limited: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "files-wanted")]
-    pub files_wanted: Option<Vec<usize>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "files-unwanted")]
-    pub files_unwanted: Option<Vec<usize>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub group: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub honors_session_limits: Option<bool>,
-
-    // Don't expose the `ids` field as it is blindly overwritten by `torrent_set`.
-    #[serde(skip_serializing_if = "Option::is_none")]
-    ids: Option<Vec<Id>>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub labels: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub location: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "peer-limit")]
-    pub peer_limit: Option<u16>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "priority-high")]
-    pub priority_high: Option<Vec<usize>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "priority-low")]
-    pub priority_low: Option<Vec<usize>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "priority-normal")]
-    pub priority_normal: Option<Vec<usize>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub queue_position: Option<usize>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub seed_idle_limit: Option<u16>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub seed_idle_mode: Option<IdleMode>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub seed_ratio_limit: Option<f64>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub seed_ratio_mode: Option<RatioMode>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub sequential_download: Option<bool>,
-    /// Download from a specific piece when [sequential download] is enabled.
-    ///
-    /// > Added in Transmission 4.1.0 (`rpc_version_semver` 6.0.0, `rpc_version`: 18)
-    ///
-    /// [sequential download]: Self::sequential_download
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub sequential_download_from_piece: Option<u64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub tracker_add: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub tracker_list: Option<TrackerList>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub tracker_remove: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub tracker_replace: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub upload_limit: Option<usize>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub upload_limited: Option<bool>,
 }
 
 #[cfg(test)]
