aa30ac98510895095f46c268ce1b6eacd2eb1758
lv10wizard <foo@example.com>
2026-03-27 19:14:06 -0700

    refactor(response): sort SessionStats fields
    * Added better(?) type TODOs.
    
        These type changes shouldn't matter in pretty much all cases, but
        may cause application code to break if they directly expect the
        old (current) types.
    
        I'm not sure these are worth changing but would feel more tidy by
        modeling the server's types better.

--- src/types/response/session_stats.rs
+++ src/types/response/session_stats.rs
@@ -3,37 +3,37 @@ use serde::Deserialize;
 #[derive(Deserialize, Default, Debug, Clone, Copy, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct SessionStats {
-    #[serde(alias = "torrent_count")]
-    pub torrent_count: i32,
     #[serde(alias = "active_torrent_count")]
-    pub active_torrent_count: i32,
-    #[serde(alias = "paused_torrent_count")]
-    pub paused_torrent_count: i32,
-    #[serde(alias = "download_speed")]
-    pub download_speed: i64,
-    #[serde(alias = "upload_speed")]
-    pub upload_speed: i64,
-    #[serde(alias = "current-stats")]
-    #[serde(alias = "current_stats")]
-    pub current_stats: Stats,
+    pub active_torrent_count: i32, // TODO: u64? u32?
     #[serde(alias = "cumulative-stats")]
     #[serde(alias = "cumulative_stats")]
     pub cumulative_stats: Stats,
+    #[serde(alias = "current-stats")]
+    #[serde(alias = "current_stats")]
+    pub current_stats: Stats,
+    #[serde(alias = "download_speed")]
+    pub download_speed: i64, // TODO: u64
+    #[serde(alias = "paused_torrent_count")]
+    pub paused_torrent_count: i32, // TODO: u64? u32?
+    #[serde(alias = "torrent_count")]
+    pub torrent_count: i32, // TODO: u64? u32?
+    #[serde(alias = "upload_speed")]
+    pub upload_speed: i64, // TODO: u64
 }
 
 #[derive(Deserialize, Default, Debug, Clone, Copy, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct Stats {
-    #[serde(alias = "files_added")]
-    pub files_added: i32,
     #[serde(alias = "downloaded_bytes")]
-    pub downloaded_bytes: i64,
-    #[serde(alias = "uploaded_bytes")]
-    pub uploaded_bytes: i64,
+    pub downloaded_bytes: i64, // TODO: u64
+    #[serde(alias = "files_added")]
+    pub files_added: i32, // TODO: u64
     #[serde(alias = "seconds_active")]
-    pub seconds_active: i64,
+    pub seconds_active: i64, // TODO: u64
     #[serde(alias = "session_count")]
-    pub session_count: Option<i32>,
+    pub session_count: Option<i32>, // Why is this an Option<_>? // TODO: u64
+    #[serde(alias = "uploaded_bytes")]
+    pub uploaded_bytes: i64, // TODO: u64
 }
 
 #[cfg(test)]
